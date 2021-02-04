e83ba6444a3aa5e830bccd4d4b37a53b881e1ca7
Don <donyang@163.com>
2021-02-04 22:21:48 +0800

    [update]modify windows delete command

--- mysql/docker-del.cmd
+++ mysql/docker-del.cmd
@@ -1,3 +1,3 @@
-#docker rm -f loamen-mysql -v
-docker rm -f -v $(docker ps -a | grep "loamen-mysql:5.7" | awk '{print $1}')
+docker rm -f loamen-mysql:5.7 -v
+#docker rm -f -v $(docker ps -a | grep "loamen-mysql:5.7" | awk '{print $1}')
 docker rmi loamen-mysql:5.7

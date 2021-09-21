bd7e538ddb06fe9b29e8f672d166dc4b7f1c67fe
don <donyang@163.com>
2021-09-21 15:52:33 +0800

    [update]update readme

--- frp/frp/startc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "开始执行frpc"
-nohup /opt/frp/frpc -c /opt/frp/conf/frpc.ini  > /opt/frp/logs/client/client.log 2>&1 &
-#/opt/frp/frpc -c /opt/frp/conf/frpc.ini
-echo "执行frpc结束"

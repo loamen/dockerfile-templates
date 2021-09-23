ff78d43332d8ccc542e0a06e8802081399c3c83e
Don <donyang@163.com>
2021-09-24 01:24:49 +0800

    [update]更新执行脚本

--- k8s/install/k8s_init_master.sh
+++ k8s/install/k8s_init_master.sh
@@ -5,26 +5,38 @@ sudo systemctl enable kubelet
 lsmod | grep br_netfilter
 
 echo "2.初始化master节点"
-sudo kubeadm init --pod-network-cidr 10.5.0.0/16 --control-plane-endpoint=vm-ubuntu-06
+echo -e "\n\n"
+echo "============="
+echo -e "\033[31m 请输入master节点的HostName: \033[0m"
+read hostName
+
+echo -e "\033[32m 执行初始化：sudo kubeadm init --pod-network-cidr 10.5.0.0/16 --control-plane-endpoint=$hostName \033[0m"
+sudo kubeadm init --pod-network-cidr 10.5.0.0/16 --control-plane-endpoint=$hostName
 #sudo kubeadm init --pod-network-cidr 10.5.0.0/16
 
+sleep 10
+
 echo "3.master节点配置"
-#mkdir -p $HOME/.kube
-#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
-#sudo echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> /etc/profile
-export KUBECONFIG=/etc/kubernetes/admin.conf
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo export KUBECONFIG=/etc/kubernetes/admin.conf
 
-echo "4.手动初始化网络（选取一种执行）"
+echo "\n\n=================\n\n"
+echo "4.初始化网络（选取一种执行）"
 
-echo "flannel网络插件"
-echo "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
-echo "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml"
+echo "安装flannel网络插件"
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+curl -C- -fLO --retry 3 https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+kubectl apply -f kube-flannel-rbac.yml
 
 echo "calico网络插件"
 echo "kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml"
-echo "kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/install"
+echo -e "kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/install\n\n\n"
 
-kubectl cluster-info
 echo "kubectl cluster-info"
-echo "kubectl get pods --all-namespaces"
+kubectl cluster-info
+
+sleep 3
+echo "执行：watch kubectl get pods --all-namespaces"
+watch kubectl get pods --all-namespaces
--- k8s/install/k8s_pull_images.sh
+++ k8s/install/k8s_pull_images.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
-# Script For Quick Pull K8S Docker Images
-# by Hellxz Zhang <hellxz001@foxmail.com>
+echo "修改docker普通用户权限"
+sudo chmod 777 /var/run/docker.sock
 
 echo "1.拉取镜像"
 
 #下面的版本号要对应
-KUBE_VERSION=v1.22.1 
+KUBE_VERSION=v1.22.2
 PAUSE_VERSION=3.5
 CORE_DNS_VERSION=1.8.4
 ETCD_VERSION=3.5.0-0
 
 # pull kubernetes images from hub.docker.com
-docker pull kubeimage/kube-proxy-amd64:$KUBE_VERSION
-docker pull kubeimage/kube-controller-manager-amd64:$KUBE_VERSION
-docker pull kubeimage/kube-apiserver-amd64:$KUBE_VERSION
-docker pull kubeimage/kube-scheduler-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$KUBE_VERSION
 # pull aliyuncs mirror docker images
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:$PAUSE_VERSION
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$CORE_DNS_VERSION
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:$ETCD_VERSION
 
 # retag to k8s.gcr.io prefix
-docker tag kubeimage/kube-proxy-amd64:$KUBE_VERSION  k8s.gcr.io/kube-proxy:$KUBE_VERSION
-docker tag kubeimage/kube-controller-manager-amd64:$KUBE_VERSION k8s.gcr.io/kube-controller-manager:$KUBE_VERSION
-docker tag kubeimage/kube-apiserver-amd64:$KUBE_VERSION k8s.gcr.io/kube-apiserver:$KUBE_VERSION
-docker tag kubeimage/kube-scheduler-amd64:$KUBE_VERSION k8s.gcr.io/kube-scheduler:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$KUBE_VERSION  k8s.gcr.io/kube-proxy:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$KUBE_VERSION k8s.gcr.io/kube-controller-manager:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$KUBE_VERSION k8s.gcr.io/kube-apiserver:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$KUBE_VERSION k8s.gcr.io/kube-scheduler:$KUBE_VERSION
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause:$PAUSE_VERSION k8s.gcr.io/pause:$PAUSE_VERSION
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$CORE_DNS_VERSION k8s.gcr.io/coredns/coredns:v$CORE_DNS_VERSION
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:$ETCD_VERSION k8s.gcr.io/etcd:$ETCD_VERSION
 
 # untag origin tag, the images won't be delete.
-docker rmi kubeimage/kube-proxy-amd64:$KUBE_VERSION
-docker rmi kubeimage/kube-controller-manager-amd64:$KUBE_VERSION
-docker rmi kubeimage/kube-apiserver-amd64:$KUBE_VERSION
-docker rmi kubeimage/kube-scheduler-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$KUBE_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/pause:$PAUSE_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$CORE_DNS_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:$ETCD_VERSION
 
-echo "所需镜像"
+echo "====================执行完毕======================"
+
+echo "所需镜像:"
 kubeadm config images list
 
-echo "已安装镜像"
+echo "已安装镜像:"
 sudo docker images
+
+echo "====如果数量不匹配请多执行几次k8s_pull_images.sh====="

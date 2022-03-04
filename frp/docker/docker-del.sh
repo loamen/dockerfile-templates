1da7cda0288959eb01f09d2a7a4e0f5d174c6ec3
don <yangdong@huimatech.com>
2022-03-04 22:09:16 +0800

    [update]update frp version to 0.39.1

--- frp/docker/docker-del.sh
+++ frp/docker/docker-del.sh
@@ -1,4 +1,4 @@
 #docker rm -f loamen-mysql -v
-docker rm -f -v $(docker ps -a | grep "loamen/frp:0.37.1" | awk '{print $1}')
-docker rmi loamen/frp:0.37.1
+docker rm -f -v $(docker ps -a | grep "loamen/frp:0.39.1" | awk '{print $1}')
+docker rmi loamen/frp:0.39.1
 docker images

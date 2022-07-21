b498b06202d61878d021a0be99261a6d93a15e3d
don <donyang@163.com>
2022-07-21 17:31:40 +0800

    [update] 升级frp版本到0.44.0

--- frp/docker/docker-build.sh
+++ frp/docker/docker-build.sh
@@ -1,3 +1,4 @@
 #docker build -f Dockerfile -t loamen/frp:0.37.1 --no-cache=true .
-docker build -f Dockerfile -t loamen/frp:0.39.1 .
+#docker build -f Dockerfile -t loamen/frp:0.39.1 .
+docker build -f Dockerfile -t loamen/frp:0.44.0 .
 docker images
